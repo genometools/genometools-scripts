a0f01cdda229fc501a1c6045a0aae9369e1cd06a
Sascha Steinbiss <ss34@sanger.ac.uk>
2015-06-25 15:17:00 +0100

    make dep installation more quiet

--- testsuite/install_dependencies.sh
+++ testsuite/install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 wget -q https://github.com/genometools/genometools/archive/master.zip
-unzip master.zip
+unzip -qq master.zip
 rm -f master.zip
 cd genometools-master
 make -j3
