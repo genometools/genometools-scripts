6fd7b72d1448cc27c6b08e747321b2b8bf73d967
Sascha Steinbiss <ss34@sanger.ac.uk>
2015-06-26 12:45:07 +0100

    fix test

--- testsuite/bequeath_tests.rb
+++ testsuite/bequeath_tests.rb
@@ -1,6 +1,6 @@
 Name "bequeath: runnable?"
 Keywords "bequeath"
 Test do
-  run_test("gt #{$cur}/gt-bequeath -h", :retval => 1)
-  grep last_stderr, 'given parent attribute to all child'
+  run_test("gt #{$cur}/gt-bequeath -h")
+  grep last_stdout, 'given parent attribute to all child'
 end
