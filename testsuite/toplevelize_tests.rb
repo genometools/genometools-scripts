346dc9d9a1fe9fe44b877c3c8b63d5f2b28162cc
Sascha Steinbiss <ss34@sanger.ac.uk>
2015-07-03 17:07:52 +0100

    add check to toplevelize tool

--- testsuite/toplevelize_tests.rb
+++ testsuite/toplevelize_tests.rb
@@ -21,3 +21,12 @@ Test do
   run("diff #{last_stdout} #{$testdata}/toplevelize_out2.gff3")
   run("diff toplevelized_foo.fasta #{$testdata}/toplevelize_out2.fasta")
 end
+
+Name "toplevelize: weird child features"
+Keywords "toplevelize"
+Test do
+  run ("cp #{$testdata}/4_genomic_dmel_RELEASE3-1.FASTA.gz ./1.fas.gz")
+  run_test("gt #{$cur}/gt-toplevelize 1.fas.gz < #{$testdata}/toplevelize_error.gff3 > out")
+  grep(last_stderr, "outside its")
+  run("diff out #{$testdata}/toplevelize_error_out.gff3")
+end
